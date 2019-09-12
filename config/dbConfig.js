d3901fbb2a129c5ad5a57a9f6518cd809d8a44f9
Shubham JIndal <shubhamjindal1234@gmail.com>
2019-09-12 14:59:41 +1000

    added docker file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,7 +5,7 @@
  'use strict';
 
 var mongo = {
-    URI: process.env.MONGO_URI || 'mongodb://localhost/User_Onboarding_Deakin',
+    URI: process.env.MONGO_URI || 'mongodb+srv://'+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@cluster0-u4lid.mongodb.net/myshout-dev?retryWrites=true&w=majority",
     //URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@localhost/"+process.env.MONGO_DBNAME_DEV,
     port: 27017
 };
