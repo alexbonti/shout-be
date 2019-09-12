4f39448a4ca91578a1acba9e46828dc0ae1d3fcf
Anirudhm9 <anirudh.m0009@gmail.com>
2019-09-12 15:44:05 +1000

    bug fixes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,7 +6,6 @@
 
 var mongo = {
     URI: process.env.MONGO_URI || 'mongodb+srv://'+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@cluster0-u4lid.mongodb.net/myshout-dev?retryWrites=true&w=majority",
-    //URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@localhost/"+process.env.MONGO_DBNAME_DEV,
     port: 27017
 };
 
