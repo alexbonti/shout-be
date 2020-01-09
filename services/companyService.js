1ef677c4f05a0eff3fbcc498edfa2814ead76056
Anirudhm9 <anirudh.m0009@gmail.com>
2020-01-09 11:05:06 +1100

    extended compnay details

--- services/companyService.js
+++ services/companyService.js
@@ -1,27 +1,46 @@
 'use strict';
 
- var Models = require('../models');
- 
- //Insert company in DB
- var createCompany = function (objToSave, callback) {
-     new Models.Company(objToSave).save(callback)
- };
- 
- //Get Company from DB
- var getCompany = function (criteria, projection, options, callback) {
-     options.lean = true;
-     Models.Company.find(criteria, projection, options, callback);
- };
- 
- //Update Company in DB
- var updateCompany = function (criteria, dataToSet, options, callback) {
+var Models = require('../models');
+
+//Insert company in DB
+var createCompany = function (objToSave, callback) {
+    new Models.Company(objToSave).save(callback)
+};
+
+//Get Company from DB
+var getCompany = function (criteria, projection, options, callback) {
+    options.lean = true;
+    Models.Company.find(criteria, projection, options, callback);
+};
+
+//Update Company in DB
+var updateCompany = function (criteria, dataToSet, options, callback) {
     options.lean = true;
     options.new = true;
     Models.Company.findOneAndUpdate(criteria, dataToSet, options, callback);
 };
 
+var getPopulatedCompanyDetails = function (
+    criteria,
+    projection,
+    populate,
+    sortOptions,
+    setOptions,
+    callback
+) {
+    Models.Company.find(criteria)
+        .select(projection)
+        .populate(populate)
+        .sort(sortOptions)
+        .exec(function (err, result) {
+            callback(err, result);
+        });
+};
+
+
 module.exports = {
     updateCompany: updateCompany,
     createCompany: createCompany,
-    getCompany: getCompany
+    getCompany: getCompany,
+    getPopulatedCompanyDetails: getPopulatedCompanyDetails
 };
